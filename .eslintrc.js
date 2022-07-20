499d23c47a25e7f315d87b4b32ed584f3dbbd937
hina <hina@hinaloe.net>
2022-07-21 04:08:32 +0900

    Use latest eslint & @nuxtjs/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,12 @@ module.exports = {
     browser: true,
     node: true,
   },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
   extends: [
     'eslint:recommended',
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or
     // `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/recommended',
+    '@nuxtjs',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
   ],
