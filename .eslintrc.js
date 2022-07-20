7b58b860b5df1d8704e06d264a5e78704b5e4b82
hina <hina@hinaloe.net>
2022-07-21 04:15:14 +0900

    Clean comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,6 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or
-    // `plugin:vue/recommended` for stricter rules.
     '@nuxtjs',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
